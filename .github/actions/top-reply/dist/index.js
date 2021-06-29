e9f190d5210fcceef62ed1fe2e39fe847bb783ae
mktcode <kontakt@markus-kottlaender.de>
2021-06-29 14:45:32 +0200

    fixed bug

--- .github/actions/top-reply/dist/index.js
+++ .github/actions/top-reply/dist/index.js
@@ -4171,7 +4171,7 @@ async function run() {
     }
     const storyNumber = foundNumber[0]
 
-    const replies = await getTweetReplies(latestStoryTweet.id, twitterBearerToken, []).filter(reply => reply.in_reply_to_user_id == accountId)
+    const replies = (await getTweetReplies(latestStoryTweet.id, twitterBearerToken, [])).filter(reply => reply.in_reply_to_user_id == accountId)
 
     if (replies.length) {
       const topReply = validReplies.sort((a, b) => b.public_metrics.like_count - a.public_metrics.like_count)[0]
