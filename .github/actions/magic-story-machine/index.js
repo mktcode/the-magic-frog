0a2f998dfbf4362f97398a82a74df95b8c297681
mktcode <kontakt@markus-kottlaender.de>
2021-06-29 13:04:51 +0200

    get image and set outputs

--- .github/actions/magic-story-machine/index.js
+++ .github/actions/magic-story-machine/index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const getTweetReplies = require('./get-tweet-replies')
+const getTweetImage = require('./get-tweet-image')
 const state = require('./../../../stories/state.json')
 
 async function run() {
@@ -9,7 +10,19 @@ async function run() {
     const validReplies = replies.filter(reply => reply.in_reply_to_user_id == state.accountId)
     if (validReplies.length) {
       const topReply = validReplies.sort((a, b) => b.public_metrics.like_count - a.public_metrics.like_count)[0]
-      core.info(`Top Reply: ${topReply.text}`)
+      const image = await getTweetImage(topReply.id, twitterBearerToken)
+      
+      core.info('Top Reply:')
+      core.info(`ID: ${topReply.id}`)
+      core.info(`Author: ${topReply.author_id}`)
+      core.info(`Text: ${topReply.text}`)
+      core.info(`Likes: ${topReply.public_metrics.like_count}`)
+      core.info(`Image: ${image}`)
+
+      core.setOutput('id', topReply.id)
+      core.setOutput('author-id', topReply.author_id)
+      core.setOutput('text', topReply.text)
+      core.setOutput('image', image)
     }
   } catch (error) {
     core.setFailed(error.message)
