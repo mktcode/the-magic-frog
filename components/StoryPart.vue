dbc2514b96e67e72dc69d0b969cf2aa1cb9488c9
mktcode <kontakt@markus-kottlaender.de>
2021-07-03 19:22:06 +0200

    fixed username link in story parts

--- components/StoryPart.vue
+++ components/StoryPart.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="{ 'my-2': showUsernames }">
     <span v-if="showUsernames" class="fw-bold username">
-      <a v-if="currentStory > 2" :href="'https://twitter.com/' + username" target="__blank">@{{ username }}:</a>
+      <a v-if="currentStory > 1" :href="'https://twitter.com/' + username" target="__blank">@{{ username }}:</a>
       <a v-else :href="'https://hive.blog/@' + username" target="__blank">@{{ username }}:</a>
     </span>
     <slot />
