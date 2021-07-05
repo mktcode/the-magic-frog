ad3da973539fdc3798b550e090e1b1d4d17e2af3
mktcode <kontakt@markus-kottlaender.de>
2021-07-05 20:48:03 +0200

    try using ssml

--- .github/actions/top-reply/dist/index.js
+++ .github/actions/top-reply/dist/index.js
@@ -4200,10 +4200,9 @@ async function run() {
       const topReply = replies.sort((a, b) => b.public_metrics.like_count - a.public_metrics.like_count)[0]
       // https://www.regextester.com/53716
       const urlRegex = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/ig
-      const htmlRegex = /<\s*\w+[^>]*>(.*?)<\s*\/\s*\w+>/ig
       const image = await getTweetImage(topReply.id, twitterBearerToken)
-      const text = topReply.text.replace(urlRegex, '').replace(htmlRegex, '$1').replace('@magicstoryfrog ', '')
-      const textClean = text.replace(/#/g, '').replace(/\n+/g, '... ').replace('"', '').replace("'", '')
+      const text = topReply.text.replace(urlRegex, '').replace('@magicstoryfrog ', '').replace(/\n/g, '<br>')
+      const textClean = text.replace(/#/g, '').replace(/"/g, '\"').replace(/'/g, '\'').replace(/<br>/g, '<break time=\"750ms\"/>')
       if (!text && !image) {
         throw Error('No text or image found!')
       }
