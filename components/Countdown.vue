3be4a4cdd4916f77cdf4ff084f2d6ccccdd0d4f8
mktcode <kontakt@markus-kottlaender.de>
2021-07-09 03:20:41 +0200

    linked "Share your ideas..." button with tweet intent.

--- components/Countdown.vue
+++ components/Countdown.vue
@@ -5,7 +5,7 @@
         The story will be continued in:
       </p>
       <h2>{{ countdown }}</h2>
-      <a :href="'https://twitter.com/magicstoryfrog/status/' + latestTweet" target="__blank" class="btn btn-lg btn-success rounded-pill mt-3">
+      <a :href="`https://twitter.com/intent/tweet?in_reply_to=${latestTweet}&text=And then the magic frog came and saved the day.`" target="__blank" class="btn btn-lg btn-success rounded-pill mt-3">
         Share your idea and vote for others!
       </a>
     </div>
