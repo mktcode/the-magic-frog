bec5ed412e3f46c39c12bf96f7b0120d65eacbd1
mktcode <kontakt@markus-kottlaender.de>
2021-07-12 17:07:33 +0200

    converted lib to common js module

--- lib.js
+++ lib.js
@@ -1,18 +1,32 @@
-import { utils as ethUtils } from 'web3'
+const { utils: ethUtils } = require('web3')
 
-export function getPotAmount (sponsors) {
+const getPotAmount = (sponsors) => {
   const potAmount = sponsors.reduce((value, sponsor) => (value + BigInt(sponsor.value)), BigInt(0))
   return Number(ethUtils.fromWei(potAmount.toString(), 'ether')) * 0.75
 }
 
-export function getPotAmountFirst (sponsors) {
+const getPotAmountFirst = (sponsors) => {
   return (getPotAmount(sponsors) * 0.5).toFixed(6).replace(/0+$/, '')
 }
 
-export function getPotAmountSecond (sponsors) {
+const getPotAmountSecond = (sponsors) => {
   return (getPotAmount(sponsors) * 0.3).toFixed(6).replace(/0+$/, '')
 }
 
-export function getPotAmountThird (sponsors) {
+const getPotAmountThird = (sponsors) => {
   return (getPotAmount(sponsors) * 0.2).toFixed(6).replace(/0+$/, '')
+}
+
+const getUsersFromStory = (storyString) => {
+  const regEx = /<story-part username="([A-Za-z0-9_]{1,15})"/g
+  const usernames = [...storyString.matchAll(regEx)].map(match => match[1])
+  return usernames
+}
+
+module.exports = {
+  getPotAmount,
+  getPotAmountFirst,
+  getPotAmountSecond,
+  getPotAmountThird,
+  getUsersFromStory
 }
